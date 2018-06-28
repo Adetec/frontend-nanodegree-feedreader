9af93a1b643fd02a35648107ef6bbcb33b9c3c8d
Adel Lassag <adetech.aadl@gmail.com>
2018-06-28 01:53:58 +0100

    add at least on entry is loaded spec

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -67,22 +67,30 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-         it('Menu changes visibilty when Icon is clicked', ()=> {
+        it('Menu changes visibilty when Icon is clicked', ()=> {
             $('a.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(false);
          
             $('a.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(true);
-         });
-
+        });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+        it('At least one entry is loaded', ()=> {
+            expect($('.entry').length).not.toBe(0);
+        })
+
+
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
